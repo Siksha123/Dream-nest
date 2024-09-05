b4dd2bfe88075b8149c69feef90ad7073426c4aa
Siksha123 <pandeyshiksha6@gmail.com>
2024-09-05 09:44:12 +0530

    dream nest project

---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,13 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+      Wherever you roam, stay in the moment and make memories that last a lifetime,live in the moment and make memories that last forever.
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
